32c45c2d92f429e2188f4db97ef74c4e066bec63
B-Matthews-S <boikiematt@gmail.com>
2022-01-13 10:11:26 +0200

    Initial Commit

---
+++ rds/rds.tf
@@ -0,0 +1,16 @@
+provider "aws"{
+    region = "af-south-1"
+    shared_credentials_file = "%HOME%/.aws/credentials"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
