5061436afb386c66c9cdecf29869e395723f9859
B-Matthews-S <boikiematt@gmail.com>
2022-01-13 10:16:05 +0200

    Update structure

---
+++ aws/ec2/ec2.tf
@@ -0,0 +1,9 @@
+provider "aws"{
+    region = "af-south-1"
+    shared_credentials_file = "%HOME%/.aws/credentials"
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0afe7f8bc76b877ea"
+  instance_type = "t3.micro"
+}
